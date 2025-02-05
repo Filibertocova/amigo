0c82e817a90b9f378d99fcf8ec94016538361b51
Filiberto <74515988+Filibertocova@users.noreply.github.com>
2025-02-05 11:21:07 -0300

    corrigindo erro ao digitar amigo

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 const nomes = []; 
-
 
 const amigoInput = document.getElementById('amigo');
 const listaAmigos = document.getElementById('listaAmigos');
 const resultado = document.getElementById('resultado');
-
 
 function adicionarAmigo() {
     const nome = amigoInput.value.trim();
@@ -26,11 +22,9 @@ function adicionarAmigo() {
     atualizarLista();
 }
 
-
 function atualizarLista() {
-    listaAmigos.innerHTML = nomes.map(nome => <li>${nome}</li>).join("");
+    listaAmigos.innerHTML = nomes.map(nome => `<li>${nome}</li>`).join("");
 }
-
 
 function sortearAmigo() {
     if (nomes.length === 0) {
@@ -40,9 +34,8 @@ function sortearAmigo() {
 
     const indiceSorteado = Math.floor(Math.random() * nomes.length);
     const nomeSorteado = nomes[indiceSorteado];
-    resultado.innerHTML = <li>Amigo secreto: ${nomeSorteado}</li>;
+    resultado.innerHTML = `<li>Amigo secreto: ${nomeSorteado}</li>`;
 }
-
 
 amigoInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
